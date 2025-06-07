daeb0e7d324a66a890dbd854bed041a777f8acc2
Namikocat <125655721+Namikocat@users.noreply.github.com>
2025-06-07 21:50:19 +0700

    Update Gensong.py

--- Gensong.py
+++ Gensong.py
@@ -3,20 +3,17 @@ from audiocraft.models import MusicGen
 from audiocraft.modules.conditioners import ClassifierFreeGuidanceDropout
 import torchaudio
 
-# โหลด MusicGen พื้นฐาน
-model = MusicGen.get_pretrained("facebook/musicgen-medium")
+model = MusicGen.get_pretrained("facebook/musicgen-medium") #โหลดโมเดลmusicgen
 
-# โหลด LM ที่ผ่านการ finetune
-model.lm.load_state_dict(torch.load("saved_models/finetuned_musicgen_lm3.pt"))
-model.lm.eval()  # ใช้กับ language model ที่ฝึกไว้
 
-# ใส่ข้อความบรรยายที่ต้องการสร้างเป็นเพลง
-descriptions = ["Thai song with saw u"]
+model.lm.load_state_dict(torch.load("saved_models/finetuned_musicgen_lm3.pt")) #โหลดโมเดลที่ finetune ไว้
+model.lm.eval() 
 
-# สร้างเพลง (waveform)
+
+descriptions = ["Thai song with saw u"] #prompt
+
 model.set_generation_params(duration=10)  # ความยาวเสียง (วินาที)
 waveforms = model.generate(descriptions)
 
-# บันทึกเป็น .wav
 torchaudio.save("generated_music4.wav", waveforms[0].cpu(), sample_rate=32000)
 print("✅ เสียงถูกสร้างและบันทึกไว้ที่: generated_music4.wav")
