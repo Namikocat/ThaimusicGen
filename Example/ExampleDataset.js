eebc2c63a8fe5dbb70164290f856daf38ee15c35
Namikocat <125655721+Namikocat@users.noreply.github.com>
2025-06-08 14:44:05 +0700

    Update ExampleDataset.js

--- Example/ExampleDataset.js
+++ Example/ExampleDataset.js
@@ -1,1 +1,8 @@
-
+  {
+    "audio": "song_clip_1.wav",
+    "description": "saw u solo traditional thai instrument"
+  },
+  {
+    "audio": "song_clip_35.wav",
+    "description": "saw u solo traditional thai instrument"
+  },
